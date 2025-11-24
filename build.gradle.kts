24882633fe276d160fb01edfc36cfd8ec4417627
wallenjos01 <wallenjos01@gmail.com>
2025-11-23 20:00:57 -0700

    Update to 1.21.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,9 @@ Utils.setupResources(project, rootProject, "fabric.mod.json")
 
 dependencies {
 
-    minecraft("com.mojang:minecraft:1.21.8")
+    minecraft("com.mojang:minecraft:1.21.10")
     mappings(loom.officialMojangMappings())
-    modImplementation("net.fabricmc:fabric-loader:0.16.14")
+    modImplementation("net.fabricmc:fabric-loader:0.18.1")
 
     compileOnly(libs.pseudonym)
     compileOnly(libs.midnightcfg)
